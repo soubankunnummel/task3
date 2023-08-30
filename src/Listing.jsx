a0247620ad6450d5bbe60fa970d7458df57e4671
soubankunnummel <91714451+soubankunnummel@users.noreply.github.com>
2023-08-30 06:36:37 +0530

    Update Listing.jsx

--- src/Listing.jsx
+++ src/Listing.jsx
@@ -32,3 +32,46 @@ function Listing() {
 }
 
 export default Listing
+
+// with async and await
+
+// import React, { useEffect, useState } from 'react';
+
+// function Listing() {
+//   const [list, setList] = useState([]);
+
+//   const fetchData = async () => {
+//     try {
+//       const response = await fetch('https://dummyjson.com/quotes');
+//       if (!response.ok) {
+//         throw new Error('Network response was not ok');
+//       }
+//       const data = await response.json();
+//       console.log(data);
+
+//       if (Array.isArray(data.quotes)) {
+//         setList(data.quotes.slice(0, 10));
+//       } else {
+//         console.log('Data is not available');
+//       }
+//     } catch (error) {
+//       console.error('Error fetching data:', error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>Data List</h1>
+//       {list.map((lists, i) => (
+//         <ul key={i}>{lists.quote}</ul>
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Listing;
+
